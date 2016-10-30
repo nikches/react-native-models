aeab30f8ad8c2d3ee8982dca2e28bf8a477360b1
nikches <nikches@angelsit.ru>
2016-10-30 22:31:44 +0300

    Fixed some bugs.

--- Model.js
+++ Model.js
@@ -1,15 +1,13 @@
-import Storage from "NullStorage";
+import Storage from "./NullStorage";
 
 export default class Model {
-    static get _idKey () {
-        return "__REACT_NATIVE_MODELS_ID__";
-    }
-
     static get _classNameKey() {
         return "__REACT_NATIVE_MODELS_CLASS_NAME__";
     }
 
-    constructor() {
+    _isObjectOrArray(value) {
+        return (!(value instanceof Model)) &&
+            (Object.prototype.toString.call(value) === "object Object" || Array.isArray(value));
     }
 
     _serializeScalar(scalar) {
@@ -23,27 +21,33 @@ export default class Model {
     _serializeIterable(iterable) {
         let data = null;
 
-        if (iterable instanceof Object) {
-            data = Object.create(null);
-            for (const key in iterable) {
-                const value = iterable[key];
-                if (value[Symbol.iterator]) {
-                    data[key] = this._serializeIterable(value);
-                } else {
-                    data[key] = this._serializeScalar(value);
-                }
-            }
-        } else {
+        if (Array.isArray(iterable)) {
             data = new Array();
             for (let i = 0; i < iterable.length; i++) {
                 const value = iterable[i];
-                if (value[Symbol.iterator]) {
+                if (this._isObjectOrArray(value)) {
                     data.push(this._serializeIterable(value));
                 } else {
                     data.push(this._serializeScalar(value));
                 }
             }
+        } else {
+            data = Object.create(null);
+            for (const key in iterable) {
+                const value = iterable[key];
+
+                if (typeof(value) === "function") {
+                    continue;
+                }
+
+                if (this._isObjectOrArray(iterable)) {
+                    data[key] = this._serializeIterable(value);
+                } else {
+                    data[key] = this._serializeScalar(value);
+                }
+            }
         }
+
         return data;
     }
 
@@ -52,13 +56,14 @@ export default class Model {
         container[Model._classNameKey] = object.constructor.name;
 
         const data = Object.create(null);
-        for (const key in this) {
-            const value = this[key];
+        for (const key in object) {
+            const value = object[key];
+
             if (typeof(value) === "function") {
                 continue;
             }
 
-            if (value[Symbol.iterator]) {
+            if (this._isObjectOrArray(value)) {
                 data[key] = this._serializeIterable(value);
             } else {
                 data[key] = this._serializeScalar(value);
@@ -70,6 +75,8 @@ export default class Model {
     }
 
     serialize() {
+        const serialized = this._serialize(this);
+        return JSON.stringify(serialized);
     }
 
     deserialize() {
