05ac6c1fe65093793215d877f05d9a17ab7c1a40
nikches <nikches@angelsit.ru>
2016-10-30 19:46:22 +0300

    Initial commit.

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "installedESLint": true,
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": 0
+    }
+};
---
+++ Model.js
@@ -0,0 +1,15 @@
+import {
+    AsyncStorage
+} from "react-native";
+
+export default class Model {
+    constructor(serializableProperties) {
+        this._serializableProperties = serializableProperties;
+    }
+
+    serialize() {
+    }
+
+    deserialize() {
+    }
+}
